51c90b2309035054a19e0d62dcf0dd98d4578e5b
meernahro <meernahro11@gmail.com>
2024-06-23 13:48:24 +0300

    chore: Add reconnection logic to SupervisorSocket handleClose method

--- houstin/rootfs/usr/src/app/src/infrastructure/sockets/supervisorSocket.ts
+++ houstin/rootfs/usr/src/app/src/infrastructure/sockets/supervisorSocket.ts
@@ -4,21 +4,17 @@ import Completer from "../utils/completer";
 import { supervisorResponse } from "../utils/interfaces";
 import Elon from "../utils/elonMuskOfLoggers";
 
-
 class SupervisorSocket {
   private static instance: SupervisorSocket;
   private socket: WebSocket | null = null;
   private supervisorConfig = config.apollo_admin;
   private completers: Map<number, Completer<any>> = new Map();
   private messageIdCounter: number = 1;
+  private reconnectInterval: number = 5000; // Interval for reconnection attempts in ms
 
   // Private constructor to prevent instantiation
   private constructor() {
-    this.startConnection().catch((error) => {
-      // console.error("Failed to start WebSocket connection:", error);
-      Elon.error("Failed to start WebSocket connection:", error);
-      
-    });
+    this.startConnection();
   }
 
   // Method to get the singleton instance
@@ -33,7 +29,6 @@ class SupervisorSocket {
   private async startConnection(): Promise<void> {
     try {
       const socketUrl = new URL(this.supervisorConfig.supervisorSocketPath);
-      // console.log(`Connecting to Supervisor at ${socketUrl.toString()}`);
       Elon.info(`Connecting to Supervisor at ${socketUrl.toString()}`);
 
       return new Promise<void>((resolve, reject) => {
@@ -47,15 +42,13 @@ class SupervisorSocket {
         );
       });
     } catch (error) {
-      // console.error("Error during Supervisor Socket connection initialization:", error);
       Elon.error("Error during Supervisor Socket connection initialization:", error);
-      throw error;
+      this.scheduleReconnect();
     }
   }
 
   // Handles the opening of the WebSocket connection
   private handleOpen(resolve: () => void) {
-    // console.log("Supervisor Socket Connection Opened");
     Elon.warn("Supervisor Socket Connection Opened");
     resolve();
   }
@@ -64,7 +57,7 @@ class SupervisorSocket {
   private async handleMessage(data: WebSocket.Data) {
     try {
       const jsonData = JSON.parse(data.toString());
-      // console.log("Message from Supervisor:", jsonData);
+      Elon.info("Message from Supervisor:", jsonData);
 
       if (jsonData.id) {
         const { id } = jsonData;
@@ -83,35 +76,38 @@ class SupervisorSocket {
           break;
       }
     } catch (error) {
-      // console.error("Error handling Supervisor Socket message:", error);
       Elon.error("Error handling Supervisor Socket message:", error);
     }
   }
 
   // Handles errors from the WebSocket connection
   private handleError(error: Error, reject: (reason?: any) => void) {
-    // console.error("Supervisor Socket Error:", error);
     Elon.error("Supervisor Socket Error:", error);
+    this.scheduleReconnect();
     reject(error);
   }
 
   // Handles the closing of the WebSocket connection
   private handleClose(code: number, reason: string) {
-    // console.log(`Supervisor Socket Closed - Code: ${code}, Reason: ${reason}`);
     Elon.warn(`Supervisor Socket Closed - Code: ${code}, Reason: ${reason}`);
     this.socket = null;
-    this.startConnection();
-    // Implement reconnection logic here if needed
+    this.scheduleReconnect();
+  }
+
+  // Schedule a reconnection attempt after a delay
+  private scheduleReconnect() {
+    setTimeout(() => {
+      Elon.warn("Attempting to reconnect to Supervisor...");
+      this.startConnection();
+    }, this.reconnectInterval);
   }
 
   // Asynchronously closes the WebSocket connection, if it is open
   public async closeConnection(): Promise<void> {
     if (this.socket) {
-      // console.log("Closing Supervisor Socket Connection");
       Elon.warn("Closing Supervisor Socket Connection");
       return new Promise<void>((resolve, reject) => {
         this.socket!.once("close", (code, reason) => {
-          // console.log(`Connection closed - Code: ${code}, Reason: ${reason}`);
           Elon.warn(`Connection closed - Code: ${code}, Reason: ${reason}`);
           resolve();
         });
